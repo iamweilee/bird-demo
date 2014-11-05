ed1795c86f754070095b1b99ee7df00034888f5b
iamweilee <383523223@qq.com>
2014-11-06 00:57:08 +0800

    include指令使用异步加载tpl

--- dep/bird.js
+++ dep/bird.js
@@ -4936,7 +4936,7 @@ define("bird.databind", [ "bird.dom", "bird.lang", "bird.array", "bird.event", "
                             var dataBind = doInclude(node, cachedTpl, model, actionId);
                             dataBind && dataBinds.push(dataBind);
                         } else {
-                            request.syncLoad(val.variable, function(data) {
+                            request.load(val.variable, function(data) {
                                 var dataBind = doInclude(node, data, model, actionId);
                                 dataBind && dataBinds.push(dataBind);
                                 lruCache.add(val.variable, data);
